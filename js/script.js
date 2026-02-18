5b2df2043b0938db6628a0ac73b1068df55482ae
Ilya <ilya.bahmat@gmail.com>
2026-02-18 03:11:15 +0300

    Initial commit: project structure

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Простой скрипт для MusicStream
+
+console.log('Добро пожаловать в MusicStream!');
+
+// Приветственное сообщение при загрузке страницы
+window.addEventListener('DOMContentLoaded', function() {
+    console.log('Страница загружена успешно');
+});
+
